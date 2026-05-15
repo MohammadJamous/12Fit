7eb388a8b477fd644e9884c48614143c94536dc8
‪Mohammad Jamous‬‏ <118446184+MohammadJamous@users.noreply.github.com>
2026-05-15 21:02:45 +0300

    Fix formatting in adminMiddleware.js

--- server/middleware/adminMiddleware.js
+++ server/middleware/adminMiddleware.js
@@ -1,5 +1,5 @@
 const adminMiddleware = (req, res, next) => {
-  .
+  
   if (!req.user || (req.user.role !== "admin" && req.user.role !== "super_admin")) {
     return res.status(403).json({ message: "Access denied" });
   }
